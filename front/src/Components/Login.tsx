48688ad2fa3d99223e4b9c3c650c35b7af593924
dbaranov7658 <dima.baranov.2016@inbox.ru>
2022-02-24 15:37:20 -0800

    tasl 1

--- front/src/Components/Login.tsx
+++ front/src/Components/Login.tsx
@@ -7,18 +7,18 @@ function Login() {
     <div style={{height: '100vh', width: '100vw'}}>
      <Row style={{height: "30%", backgroundColor: "red"}} >
         <Button>
-            test
+            testttttt
         </Button>
      </Row>
         <Row style={{height: "40%"}}>
             <Col span={6} style={{backgroundColor: "green"}}>
-
+                test1
             </Col>
             <Col span={12} style={{backgroundColor: "blue"}}>
-
+                test2
             </Col>
             <Col span={6} style={{backgroundColor: "yellow"}}>
-
+            test3
             </Col>
 
         </Row>
