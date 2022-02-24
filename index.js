e7b4b36bd3fe9d07816e9b27bcb7c3a25693e1f5
dbaranov7658 <dima.baranov.2016@inbox.ru>
2022-02-23 18:12:46 -0800

    test

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+const path = require('path')
+
+const  reactBuild = path.join(__dirname, 'front', 'build')
+
+app.use(express.static(reactBuild))
+app.get('/', async(req, res) => {
+    res.send({message: 'test'})
+})
+
+app.get('*', async(req, res) => {
+        res.sendFile(path.join(reactBuild, 'index.html'))
+})
+
+
+
+app.listen(PORT, ()=>{console.log('server is running on ' + PORT)})
