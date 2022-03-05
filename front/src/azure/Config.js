a3e90ebecb85ea4b55ae9c2e17606639c67944b6
Misha Sinitcyn <mishasin11@gmail.com>
2022-03-05 10:23:03 -0800

    implemented access to user information

---
+++ front/src/azure/Config.js
@@ -0,0 +1,8 @@
+export const config = {
+    appId: 'b99af29c-5b0a-4990-8970-c5f443296bb8',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+        'user.read'
+    ],
+    authority:'https://login.microsoftonline.com/consumers/'
+};
