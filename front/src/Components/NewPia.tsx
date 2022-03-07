2ef9715dd7cad076f8032ca8014fd7e2dda56c59
Abdinajib idle <Abdul-pc@MacBook-Air-7.local>
2022-03-06 21:28:38 -0800

    Switched to using functions with hook instead of classes to sort out bugs

--- front/src/Components/NewPia.tsx
+++ front/src/Components/NewPia.tsx
@@ -1,88 +1,62 @@
-
-import '../CSS/App.css';
-import React from 'react';
-import FormItem from 'antd/lib/form/FormItem';
+import React, { useState } from "react";
 import {
-    Form,Input,Button,Radio,Select,Cascader,
-  } from 'antd';
-  const { TextArea } = Input;
-export default class NewPia extends React.Component<any, any>  {
-  constructor(props:any) {
-    super(props);
-    this.state = {
-      ProjectName: '',
-      sponsoringGroup:'',
-      projDescription:'',
-      
-    };
-    this.handleChange=this.handleChange.bind(this)
-    this.handleSubmit=this.handleSubmit.bind(this)
-    
-  }
-  handleChange=(event) =>{
-    this.setState({
-    [event.target.name] :event.target.value
-    })}
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+  Form,Button,Radio,Cascader, Input, Select,
+} from 'antd';
+
+const {TextArea}=Input
+
+export default function NewPia() {
+  const [user, setUser] = useState({ fullname: "", sponsors: "", Description:  "",Disclosure:"",Purpouse:"",Necessity:"",accountableIndividuals:"", });
+
+  const handleChange = (event) => {
+  console.log(event.value)
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
     event.preventDefault();
-  }
-  render() {
-    return (
-    <div className='newpiaContainer'>
-      <Form onSubmitCapture={this.handleSubmit}>
-      <FormItem>
-        <label>ProjectName</label><br />
-          <Input type="text" name="projectName" value={this.state.ProjectName} onChange={this.handleChange}></Input>
+  };
+  return (
+  <div className="newpiaContainer">
+    <Form onSubmitCapture={handleSubmit}
+    >
+ 
+      <Form.Item htmlFor="ProjName" label="Project Name"> 
+  
+      <Input type="text" name="ProjName" onChange={handleChange}  />
+      </Form.Item>
+      
+      <label htmlFor="sponsors">Sponsoring Business Unit</label> <br />
+      <Form.Item>
+      <Select id="sponsors" onChange={handleChange} >
+      <Select.Option value="default">default</Select.Option>
+      <Select.Option value="saab">Saab</Select.Option>
+      <Select.Option value="fiat">Fiat</Select.Option>
+      <Select.Option value="audi">Audi</Select.Option>
+      </Select>
+      </Form.Item>
+     
+      <Form.Item  htmlFor="Description" label="Project description">
+      <br />
+      <TextArea  name="Description" onChange={handleChange} />
+     </Form.Item>
+     <Form.Item label="Is it necessary for the purpose of the project that personal information be collected, used or disclosed?"htmlFor="Necessity">
+     <br />
+     <br />
+     
+     <Radio.Group onChange={handleChange}>
+      <Radio value={1}>Yes</Radio>
+      <Radio value={2}>No</Radio>
+    </Radio.Group>
+   
+    </Form.Item>
+
+<Form.Item label="Which “purpose” in S2.3 of the FortisBC Privacy Policy applies to this project? " name="Purpouse" htmlFor="Purpouse"> 
           <br />
-          </FormItem>
-          
-          <FormItem>
-          <label>sponsoringGroup</label><br />
-          <Select value={this.state.sponsoringGroup} onChange={this.handleChange}>
-          <Select.Option value="default">Default</Select.Option>
-          <Select.Option value="addNew">Add new</Select.Option>
-          </Select>
-          </FormItem>
-          
-          <br />
-          
-          <FormItem>
-          <label>Description</label><br />
-          <TextArea name="projDescription" value={this.state.Description} onChange={this.handleChange}/>
-          </FormItem>
-          <br />
-          
-          
-          <FormItem>
-          <label>is it necessary for the purpouse of the project that personal information be collected, used or disclosed</label><br />
-          {/* {
-          this.state.checkbox ?
-          
-          :
-          null
-          } */}
-          <Radio value={1}>Yes</Radio>
-          <Radio value={2}>No</Radio>
-          </FormItem>
-          <br />
-          
-          <FormItem>
-          <label>List the individuals accountable for the personal information</label><br />
-          <Select value={this.state.value} >
-          <Select.Option value="Demo">Demo</Select.Option>
-          <Select.Option>AddNew</Select.Option>
-          </Select>
-          </FormItem>
-          <br />
-          <Form.Item label="Which “purpose” in S2.3 of the FortisBC Privacy Policy applies to this project? "> 
-          <br />
-          <Cascader
+          <Cascader id="Purpouse" onChange={handleChange}
             options={[
               {
                 value: 'Demo1',
                 label: 'Demo1',
-
               },
               {
                 value: 'Demo2',
@@ -93,26 +67,28 @@ export default class NewPia extends React.Component<any, any>  {
                 label: 'Demo3',
               },
             ]}
-
           />
         </Form.Item>
+        <Form.Item label="List the individuals accountable for the personal information" htmlFor="accountableIndividuals">
+        <TextArea id="accountableIndividuals" name="accountableIndividuals" onChange={handleChange} />
+        
+        </Form.Item>
+        <Form.Item label="is any information being disclosed or stored outside of Canada as part of this project? "htmlFor="Disclosure"name="Disclosure" 
+          rules={[{ required: true, message: "Please select an option!" }]}>
           <br />
-          <FormItem>
-          <label>is any information being disclosed or stored outside of Canada as part of this project? </label><br />
-          <Radio value={1} name="yes" id="y">Yes</Radio>
-          <Radio value={2}name="yes" id="n">No</Radio>
-          
-          </FormItem>
-    <br />
-    <FormItem>
-        <button type="submit" name="submit">Submit</button>
-        </FormItem>
-      </Form>
-      </div>
-    );
-  }
+      <Radio.Group onChange={handleChange}>
+      <Radio value={1}>Yes</Radio>
+      <Radio value={2}>No</Radio>
+    </Radio.Group>
+   
+   
+ 
+     </Form.Item>
+
+<div className="btn"><Button type="default" style={{background:"#FFC82C",color:"black"}}>Submit</Button>
+</div>
+
+    </Form>
+    </div>
+  );
 }
-
-
-
-
