11ee4ae832f4eaef050b8e454125f3706d85f96d
dbaranov7658 <dima.baranov.2016@inbox.ru>
2022-03-10 01:00:15 -0800

    iteration 1 test3

--- front/src/azure/Config.js
+++ front/src/azure/Config.js
@@ -1,6 +1,6 @@
 export const config = {
     appId: 'b99af29c-5b0a-4990-8970-c5f443296bb8',
-    redirectUri: 'https://fortis-bc-pia.herokuapp.com',
+    redirectUri: 'fortis-bc-pia.herokuapp.com',
     scopes: [
         'user.read'
     ],
