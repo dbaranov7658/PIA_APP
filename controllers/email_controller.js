b4c8f2e229b6d6862a7223134fdba60874dad0c6
n-maido <nhi_maido@outlook.com>
2022-03-17 23:22:33 -0700

    abstract emailNewPia into function

--- controllers/email_controller.js
+++ controllers/email_controller.js
@@ -24,34 +24,17 @@ transporter.verify(function (error, success) {
     }
 });
 
-
-/* 
- * @route  v1/auth/login 
- * @type   POST 
- * @access public
- * url: http:localhost:5000/v1/email/emailNewPia
- */
-exports.emailNewPia = async (req,res) => {
-    const user_email = req.params.user_email.substring(1);
-    const pia_url = "http://localhost:3000"
-    const event_msg = `A new Privacy Impact Assessment has been submitted by ${user_email}.`
-
+async function sendEmail(req, res, pia_url, event_msg, options) {
     try {
-
+        // remove controllers from __dirname
         let _dirnames_arr = __dirname.split("/")
         let c_index = _dirnames_arr.indexOf("controllers")
         _dirnames_arr.splice(c_index, 1);
         const _abs_dir = _dirnames_arr.join("/")
 
         let data = await ejs.renderFile(_abs_dir + "/views/email_template.ejs", { event_msg: event_msg, pia_url: pia_url });
-
-        const options = {
-            from: process.env.NOTIF_EMAIL_USER,
-            to: "POFORTIS@outlook.com",
-            subject: "New PIA",
-            text: `A new Privacy Impact Assessment has been submitted by ${user_email}. Click to view: http://localhost:3000`,
-            html: data
-        }
+        options.html = data;
+        console.log(options);
 
         let result = await transporter.sendMail(options);
         console.log(result);
@@ -61,11 +44,30 @@ exports.emailNewPia = async (req,res) => {
         })    
         
     } catch(err) {
-
         res.json({
             status: false,
             message: 'Something went wrong'
         })
         console.log(err);
     }
+}
+
+
+/* 
+ * @route  v1/email/emailNewPia/:user_email
+ * @type   POST 
+ * @access public
+ */
+exports.emailNewPia = (req,res) => {
+    const user_email = req.params.user_email.substring(1);
+    const pia_url = "http://localhost:3000"
+    const event_msg = `A new Privacy Impact Assessment has been submitted by ${user_email}.`
+    const recipient = "POFORTIS@outlook.com"
+    const options = {
+        from: process.env.NOTIF_EMAIL_USER,
+        to: recipient,
+        subject: "New PIA",
+        text: `A new Privacy Impact Assessment has been submitted by ${user_email}. Click to view: ${pia_url}`, // Fallback message
+    }
+    sendEmail(req, res, pia_url, event_msg, options);
 }
