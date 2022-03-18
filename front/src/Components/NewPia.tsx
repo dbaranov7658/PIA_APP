062014e053f58105cfa655990dec19daf9969329
Abdinajib idle <Abdul-pc@d192-075-240-190.wireless.sfu.ca>
2022-03-18 16:22:56 -0700

    updated newPIA

--- front/src/Components/NewPia.tsx
+++ front/src/Components/NewPia.tsx
@@ -1,11 +1,12 @@
-
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import classicEditor from '@ckeditor/ckeditor5-build-classic'
 import {
-    Form, Button, Radio, Cascader, Input, Select, Row,
+    Form, Button, Radio,  Input, Select, Row,
 } from 'antd';
 import '../CSS/App.css';
 import * as React from "react";
 import {Link} from "react-router-dom";
-const {TextArea} = Input
 
 
 interface State{
@@ -16,25 +17,32 @@ interface State{
   purpose: string
   individualsInfo: string
   isDisclosed: boolean
+  data:string
 }
 
-export default class NewPia extends React.Component<any, State>{
+
+export default class  NewPia extends React.Component<any, State>{
           constructor(props: any){
               super(props);
-              this.state ={
+              this.state = {
                   projectName: "",
                   sponsoringBusinessUnit: "",
                   projectDescription: "",
                   isCollected: null,
                   purpose: "",
                   individualsInfo: "",
-                  isDisclosed: null
+                  isDisclosed: null,
+                  data: "",
+
               };
+              this.onSubmit=this.onSubmit.bind(this)
+              
           }
 
-        onChangeProjName = (event) => {
-              this.setState({projectName: event.target.value})
-        }
+        // onChangeProjName = (event) => {
+        //       this.setState({projectName: event.target.value})
+        //     alert("Hey")
+        // }
         onChangeSponsoringBusinessUnit = (event) => {
                 this.setState({sponsoringBusinessUnit: event})
     }
@@ -47,93 +55,121 @@ export default class NewPia extends React.Component<any, State>{
          onChangePurpose = (event) => {
                  this.setState({purpose: event})
     }
-         onChangeIndividualsInfo = (event) => {
-                 this.setState({individualsInfo: event.target.value})
-    }
+         onChangeIndividualsInfo (individualsInfo, editor) {
+            this.setState({ individualsInfo });
+           
+          }
          onChangeIsDisclosed = (event) => {
-        this.setState({isDisclosed: event.target.value})
+        this.setState({isDisclosed: event.target.value==='1'})
     }
+    handleDescriptionDataChange
+    (data, editor) {
+        this.setState({ data });
+       
+      }
+  
 
+    onSubmit=(e)=> {
 
-
+              e.preventDefault();
+             console.log(this.state.purpose)
+    }
+ 
           render(){
               return (
                   <div className="newpiaContainer">
-                      <Form
+                      <Form onSubmitCapture={this.onSubmit}
                       >
-                          <Form.Item htmlFor="ProjName" label="Project Name">
-
-                              <Input onChange={this.onChangeProjName} type="text" name="ProjName" />
+                      <label htmlFor="Project Name">Project Name</label>
+                      <br />
+                          <Form.Item>
+                              <Input value={this.state.projectName} onChange={e=>this.setState({projectName:e.target.value})} type="text" name="ProjName" />
                           </Form.Item>
 
                           <label htmlFor="sponsors">Sponsoring Business Unit</label> <br/>
                           <Form.Item>
-                              <Select id="sponsors" onChange={this.onChangeSponsoringBusinessUnit} >
-                                  <Select.Option value="default">default</Select.Option>
-                                  <Select.Option value="saab">Saab</Select.Option>
-                                  <Select.Option value="fiat">Fiat</Select.Option>
-                                  <Select.Option value="audi">Audi</Select.Option>
+                              <Select id="sponsors" value={this.state.sponsoringBusinessUnit} onChange={this.onChangeSponsoringBusinessUnit} >
+                                  <Select.Option value="Demo1">Demo1</Select.Option>
+                                  <Select.Option value="Demo2">Demo2</Select.Option>
+                                  <Select.Option value="Demo3">Demo3</Select.Option>
+                                  <Select.Option value="Demo4">Demo4</Select.Option>
                               </Select>
                           </Form.Item>
 
-                          <Form.Item htmlFor="Description" label="Project description">
+                          
+                          <label htmlFor="Description">Project description</label>
                               <br/>
-                              <TextArea name="Description" onChange={this.onChangeProjectDescription} />
-                          </Form.Item>
-                          <Form.Item
-                              label="Is it necessary for the purpose of the project that personal information be collected, used or disclosed?"
-                              htmlFor="Necessity">
-                              <br/>
-                              <br/>
+                              {/* <TextArea name="Description" onChange={this.onChangeProjectDescription} /> */}
+                              <CKEditor
+                          editor={classicEditor}
+                          value={this.state.data}
+          init={{
+            height: 200,
+            menubar: false
+          }}
+          onChange={ ( event, editor ) => {
+            const data = editor.getData();
+            console.log( { event, editor, data } );
+        } 
+          }
+                        
+                          />
+                          <br />
+                         
+                         <label htmlFor="Necessity">Is it necessary for the purpose of the project that personal information be collected, used or disclosed?</label>
+                         <br />
+                          <Form.Item>
+                              
 
                               <Radio.Group onChange={this.onChangeIsCollected}>
+                                  <Radio value={'Yes'}>Yes</Radio>
+                                  <Radio value={'NO'}>No</Radio>
+                              </Radio.Group>
+                              {this.state.isCollected ?
+                              <Input/>
+                              :
+                              null
+                              }
+
+                          </Form.Item>
+                            <br />
+                        <label htmlFor="Purpouse">Which “purpose” in S2.3 of the FortisBC Privacy Policy applies to this project? </label>
+                          <Form.Item>
+                              <Select id="purpose" value={this.state.purpose} onChange={this.onChangePurpose} >
+                                  <Select.Option value="Demo1">Demo1</Select.Option>
+                                  <Select.Option value="Demo2">Demo2</Select.Option>
+                              </Select>
+                          </Form.Item>
+                       <label htmlFor="accountableIndividuals">List the individuals accountable for the personal information</label>
+                       <br />
+                                    
+                              {/* <TextArea id="accountableIndividuals" name="accountableIndividuals" onChange={this.onChangeIndividualsInfo} /> */}
+                              <CKEditor
+                          editor={classicEditor}
+                          value={this.state.individualsInfo}
+                          init={{
+                            height: 200,
+                            menubar: false
+                          }}
+                          onEditorChange={this.onChangeIndividualsInfo
+                          }/>
+                     <br />
+                     
+                     <label htmlFor="Disclosure" >is any information being disclosed or stored outside of Canada as part of this project? </label>
+                          <Form.Item>
+                              <br/>
+                              <Radio.Group onChange={this.onChangeIsDisclosed }>
                                   <Radio value={'1'}>Yes</Radio>
                                   <Radio value={'2'}>No</Radio>
                               </Radio.Group>
 
-                          </Form.Item>
-
-                          <Form.Item
-                              label="Which “purpose” in S2.3 of the FortisBC Privacy Policy applies to this project? "
-                              name="Purpouse" htmlFor="Purpouse">
-                              <br/>
-                              <Cascader id="Purpouse" onChange={this.onChangePurpose}
-                                        options={[
-                                            {
-                                                value: 'Demo1',
-                                                label: 'Demo1',
-                                            },
-                                            {
-                                                value: 'Demo2',
-                                                label: 'Demo2',
-                                            },
-                                            {
-                                                value: 'Demo3',
-                                                label: 'Demo3',
-                                            },
-                                        ]}
-                              />
-                          </Form.Item>
-                          <Form.Item label="List the individuals accountable for the personal information"
-                                     htmlFor="accountableIndividuals">
-                              <TextArea id="accountableIndividuals" name="accountableIndividuals" onChange={this.onChangeIndividualsInfo} />
 
                           </Form.Item>
-                          <Form.Item
-                              label="is any information being disclosed or stored outside of Canada as part of this project? "
-                              htmlFor="Disclosure" name="Disclosure"
-                              rules={[{required: true, message: "Please select an option!"}]}>
-                              <br/>
-                              <Radio.Group onChange={this.onChangeIsDisclosed }>
-                                  <Radio value={1}>Yes</Radio>
-                                  <Radio value={2}>No</Radio>
-                              </Radio.Group>
-
-
-                          </Form.Item>
+                        
+                      
                         <Row>
                             <div className="btn">
-                                <Button type="default" onClick={() => {}}
+                                <Button type="default" onClick={e=>this.onSubmit(e)}
                                         style={{background: "#FFC82C", color: "black"}}>Submit</Button>
                             </div>
                             <div className="btn" style={{paddingLeft: "15px"}}>
@@ -143,8 +179,6 @@ export default class NewPia extends React.Component<any, State>{
                                 </Link>
                             </div>
                         </Row>
-
-
                       </Form>
                   </div>
               );
